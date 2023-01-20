16b51b726727315101b03eb023f33bbab0330702
MH <mh6698@vt.edu>
2023-01-20 18:58:48 -0500

    Add files via upload

---
+++ generate_song.py
@@ -0,0 +1,46 @@
+import youtube_dl
+import sys
+import os
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def execute():
+    if sys.argv[1] == '-ff':
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'cachedir': False,
+            'outtmpl': dir_path + f'/{sys.argv[2]}.mp3',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            video = sys.argv[3:]
+            ydl.download(video)
+
+        for root, dirs, files in os.walk(dir_path):
+            for file in files:
+                if file.endswith('.mp3'):
+                    os.system('ffmpeg -i ' + file + ' -filter:a "atempo=1.5" -vn new_' + file)
+                    os.remove(file)
+    else:
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'cachedir': False,
+            'outtmpl': dir_path + f'/{sys.argv[1]}.mp3',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            video = sys.argv[2:]
+            ydl.download(video)
+
+if __name__=='__main__':
+    execute()
