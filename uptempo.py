04459d51aa332a4207ac2fd4f18c7d50fb224a4d
MH <mh6698@vt.edu>
2024-12-17 23:11:36 -0500

    updated uptempo.py

--- uptempo.py
+++ uptempo.py
@@ -6,6 +6,5 @@ dir_path = os.path.dirname(os.path.realpath(sys.argv[1]))
 for root, dirs, files in os.walk(dir_path):
     for file in files:
         if file.endswith(f'.m4a'):
-            print(file)
-            os.system('ffmpeg -i ' + dir_path + file + ' -filter:a "atempo=1.5" -vn "nc_' + file)
-            # os.remove(file)
+            os.system('ffmpeg -i ' + 'C:\\Users\\MH\\Documents\\Coding\\MP3Ripper\\P3R\\' + file + ' -filter:a "atempo=1.5" -vn "nc_' + file)
+            os.remove('C:\\Users\\MH\\Documents\\Coding\\MP3Ripper\\P3R\\' + file)
